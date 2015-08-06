bacfaedec0ae4c746789bdec3faed5481451a80d
Carlos Eduardo Seo <cseo@linux.vnet.ibm.com>
2015-08-06 15:54:02 -0300

    Added ppc-specific implementation of dl-support.c.
    	* sysdeps/unix/sysv/linux/powerpc/dl-support.c: New file.
    	  Added ppc-specific implementation to provide support for
    	  _dl_platform in the static case.

---
+++ sysdeps/unix/sysv/linux/powerpc/dl-support.c
@@ -0,0 +1,25 @@
+/* Operating system support for dynamic linking code in static glibc.
+   Linux/PPC version.
+   Copyright (C) 2015 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#define DL_PLATFORM_AUXV				\
+	case AT_PLATFORM:				\
+	  GLRO(dl_platform) = (void *) av->a_un.a_val;	\
+	  break;
+
+#include <elf/dl-support.c>
